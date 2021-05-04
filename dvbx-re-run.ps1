f457659380e28294c520bdc8c5e06e6453a79a23
NPowDev <npowdev@users.noreply.github.com>
2021-05-04 14:20:26 +0200

    chore: Updating temp-dvbx-debug-run.ps1
    Add output of $DVBX.
    
    feat: Update dvbx-re-run.ps1 and feature ready

--- dvbx-re-run.ps1
+++ dvbx-re-run.ps1
@@ -14,47 +14,61 @@ Write-Output ("Current path is: $($PWD)")
 Write-Output ("Project path is: $($DVBX_WorkRoot)")
 
 $DVBX
-
-exit 1
-
-# Resolve devilbox server path.
-$Dvbx_Server_Path = (Resolve-Path -Path (Join-Path -Path $Dvbx_Project_Path -ChildPath $DVBX_CFG_SRV_REL_PATH -ea SilentlyContinue) -EA SilentlyContinue)
-if (! $?) { Write-Error ("Server path invalid."); exit 1 }
-if (! (Test-Path -Path $Dvbx_Server_Path)) { Write-Error ('Server path not found.'); exit 1 }
-
-# Go to devilbox server.
-Write-Output ("Go inside Devilbox in: $($Dvbx_Server_Path)")
-Push-Location -Path ($Dvbx_Server_Path) -ErrorAction SilentlyContinue
-if (! $?) { Write-Error ("Failed to go to server path '$($Dvbx_Server_Path)'."); exit 1 }
 Write-Output ("")
 
-# Do docker container work: stop, remove, start.
+# Go to the work root directory.
+Push-Location -LiteralPath $DVBX_WorkRoot -EA SilentlyContinue
+if (! $?) { throw [System.IO.IOException]::new("Work root path not accessible.") }
 try {
-    Write-Output ("Stop containers:")
-    docker-compose.exe stop
-    if (!$? -or ($LASTEXITCODE -ne 0)) {
-        throw ("Failed to stop containers. Abort!")
+    # Resolve devilbox server path.
+    Write-Output ("Devilbox path is set as: $($DVBX.DevilboxPath)")
+    $Dvbx_Server_Path = (Resolve-Path -LiteralPath ($DVBX.DevilboxPath) -EA SilentlyContinue)
+    if (! $?) { throw [System.IO.IOException]::new("Server path error.") }
+    if (! (Test-Path -Path $Dvbx_Server_Path)) { 
+        throw [System.IO.IOException]::new('Server path not found.') 
     }
-    Write-Output ("")
+    Write-Output ("Devilbox path resolve to: $(Resolve-Path -LiteralPath ($Dvbx_Server_Path))")
     
-    Write-Output ("Remove containers:")
-    docker-compose.exe rm -f
-    if (!$? -or ($LASTEXITCODE -ne 0)) {
-        throw ("Failed to run docker-compose rm. Abort!")
-        "Failed to remove containers. Abort!"
+    # Go to devilbox server.
+    Write-Output ("Go inside Devilbox in: $($Dvbx_Server_Path)")
+    Push-Location -Path ($Dvbx_Server_Path) -ErrorAction SilentlyContinue
+    if (! $?) { 
+        throw [System.IO.IOException]::new("Failed to go to server path '$($Dvbx_Server_Path)'.") 
     }
-    Write-Output ("")
     
-    Write-Output ("Start containers:")
-    docker-compose.exe "up" -d $DVBX_CFG_SERVICES
-    if (!$? -or ($LASTEXITCODE -ne 0)) {
-        throw ("Failed to start Devilbox. Abort!")
+    # Do docker container work: stop, remove, start.
+    try {
+        Write-Output ("")
+        Write-Output ("Stop containers:")
+        docker-compose.exe stop
+        if (!$? -or ($LASTEXITCODE -ne 0)) {
+            throw ("Failed to stop containers. Abort!")
+        }
+        Write-Output ("")
+        
+        Write-Output ("Remove containers:")
+        docker-compose.exe rm -f
+        if (!$? -or ($LASTEXITCODE -ne 0)) {
+            throw ("Failed to remove containers. Abort!")
+        }
+        Write-Output ("")
+        
+        Write-Output ("Start containers:")
+        docker-compose.exe "up" -d $DVBX.LoadServices
+        if (!$? -or ($LASTEXITCODE -ne 0)) {
+            throw ("Failed to start Devilbox. Abort!")
+        }
+        Write-Output ("")
     }
-    Write-Output ("")
+    # Go back to last current directory.
+    finally {
+        # Write-Output ("Go Back to last place in: $($Dvbx_Pwd)")
+        Pop-Location
+    }
 }
-# Go back to last current directory.
+# Go to directory back as like started.
 finally {
-    Write-Output ("Go Back to last place in: $($Dvbx_Pwd)")
-    Pop-Location
+    Pop-Location -ErrorAction SilentlyContinue
 }
+
 Write-Output ("Ready.")
