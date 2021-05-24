b83aaad9379ee6ea71baa921c6a4fcab38646812
NPowDev <npowdev@users.noreply.github.com>
2021-05-24 18:08:30 +0200

    feat: Update dvbx-up.ps1
    Add debug option to launch dvbx-up.ps1

--- dvbx-up.ps1
+++ dvbx-up.ps1
@@ -2,9 +2,48 @@
 # Load Base Tool: DvbxUtiliy
 ########################################################################
 . "$($PSScriptRoot)\DvbxUtility\DvbxUtility.ps1" $PSScriptRoot
-if (!$?) { Write-Error -Message "Load of DvbxUtiliy may has failed!" -EA Stop }
 
 ########################################################################
 # Script code that use DvbxUtility start here
 ########################################################################
 
+# Get some paths.
+Write-Output ("")
+Write-Output ("Project '$(Split-Path $DVBX_WorkRoot -Leaf )' at '$($PWD)'")
+
+# Go to the work root directory.
+Push-Location -LiteralPath $DVBX_WorkRoot -EA SilentlyContinue
+if (! $?) { throw [System.IO.IOException]::new("Project root path not accessible.") }
+try {
+    # Resolve devilbox server path.
+    Write-Output ("Devilbox path: '$($DVBX.DevilboxPath)'")
+    $DVBX_ServerPath = (Resolve-Path -LiteralPath ($DVBX.DevilboxPath) -EA SilentlyContinue)
+    if (! $?) { throw [System.IO.IOException]::new("Server path not resolvable.") }
+    Write-Output ("Go to Devilbox: '$($DVBX_ServerPath)'")
+
+    # Test path.
+    if (! (Test-Path -Path $DVBX_ServerPath -PathType Container)) { 
+        throw [System.IO.IOException]::new('The resolved Devilbox path not a valid directory.') 
+    }
+    
+    # Go to devilbox server.
+    Set-Location -Path ($DVBX_ServerPath) -EA SilentlyContinue
+    if (! $?) { 
+        throw [System.IO.IOException]::new("Failed to go to Devilbox path '$($DVBX_ServerPath)'.") 
+    }
+    
+    # Do docker container: up.
+    Write-Output ("Starts up Devilbox...")
+    docker-compose.exe "up" -d $DVBX.LoadServices
+    if (!$? -or ($LASTEXITCODE -ne 0)) {
+        throw ("Failed to start up Devilbox containers. Abort!")
+    }
+    Write-Output ("")
+}
+# Go back to start directory.
+finally {
+    Pop-Location -EA SilentlyContinue
+}
+
+Write-Output ("Done.")
+Write-Output ("")
